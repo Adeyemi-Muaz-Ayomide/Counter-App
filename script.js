39f5569f76380d15060336cd58a7480022420c66
iAmMuadh <adeyemimuaz1@gmail.com>
2023-02-24 23:58:45 +0100

    wrote an IF statement to increase, reset and decrease our count

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ let btns = document.querySelectorAll('.btn');
 btns.forEach(function(btn) {
     btn.addEventListener('click' , function(e) {
         const value = e.currentTarget.classList;
-        
+        if(value.contains('decrease')) {
+            count--;
+        } else if(value.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        };
+
+        countEl.textContent = count;
     })
 })
