048e07a4d84fc11d58474e8f6894d122d16f56bf
iAmMuadh <adeyemimuaz1@gmail.com>
2023-02-24 23:51:23 +0100

    used the forEach method to access the btn and added an event listner

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let count = 0;
 let countEl = document.querySelector('.counting');
 let btns = document.querySelectorAll('.btn');
 
+btns.forEach(function(btn) {
+    btn.addEventListener('click' , function() {
+        
+    })
+})
